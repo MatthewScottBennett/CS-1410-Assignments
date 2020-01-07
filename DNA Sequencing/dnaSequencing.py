233f151adda0bef9df2e7d346fa514c789972840
MatthewScottBennett <matt.bennett715@gmail.com>
2020-01-07 14:12:53 -0700

    Added logic to functions: returnFirstStrings, strandsAreNotEmpty, and strandsAreEqualLengths

--- DNA Sequencing/dnaSequencing.py
+++ DNA Sequencing/dnaSequencing.py
@@ -10,7 +10,43 @@ def fileToList(filename):
     return files_lines
 
 def returnFirstString(strings):
+    if strings == []:
+        return ''
+    else:
+        return strings[0]
+
+def strandsAreNotEmpty(strand1, strand2):
+    strand1length = len(strand1)
+    strand2length = len(strand2)
+    if strand1length > 0 and strand2length > 0:
+        return True
+    else:
+        return False
+
+def strandsAreEqualLengths(strand1, strand2):
+    strand1length = len(strand1)
+    strand2length = len(strand2)
+    if strand1length == strand2length:
+        return True
+    else:
+        return False
+
+def candidateOverlapsTarget(target, canadite, overlap):
+
+    i = 0
+    while i <
 
 
+    return
 
-    return 0
+def findLargestOverlap():
+    return
+
+def findBestCandidate():
+    return
+
+def joinTwoStrands():
+    return
+
+def main():
+    return
