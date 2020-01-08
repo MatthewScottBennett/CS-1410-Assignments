7df9ae49b5772b5c8f790de691c4c4af69ac57b8
MatthewScottBennett <matt.bennett715@gmail.com>
2020-01-08 10:21:49 -0700

    Finished DNA Sequencing Added Logic to functions: candidateOverlapsTarget, findLargestOverlap, findBestCandidate, joinTwoStrands, and main

--- DNA Sequencing/dnaSequencing.py
+++ DNA Sequencing/dnaSequencing.py
@@ -1,4 +1,7 @@
+# Matt Bennett CS 1410 Mon-Wed-Fri 1:00pm-1:50pm
+
 import os
+
 
 def fileToList(filename):
     files_lines = []
@@ -9,11 +12,13 @@ def fileToList(filename):
         file.close()
     return files_lines
 
+
 def returnFirstString(strings):
     if strings == []:
         return ''
     else:
         return strings[0]
+
 
 def strandsAreNotEmpty(strand1, strand2):
     strand1length = len(strand1)
@@ -23,6 +28,7 @@ def strandsAreNotEmpty(strand1, strand2):
     else:
         return False
 
+
 def strandsAreEqualLengths(strand1, strand2):
     strand1length = len(strand1)
     strand2length = len(strand2)
@@ -31,22 +37,58 @@ def strandsAreEqualLengths(strand1, strand2):
     else:
         return False
 
-def candidateOverlapsTarget(target, canadite, overlap):
 
-    i = 0
-    while i <
+def candidateOverlapsTarget(target, candidate, overlap):
+    if candidate[0:overlap] == target[-overlap:]:
+        return True
+    else:
+        return False
 
 
-    return
+def findLargestOverlap(target, candidate):
+    if strandsAreEqualLengths(target, candidate) and strandsAreNotEmpty(target, candidate):
+        length = len(target)
+        largestOverlap = 0
+        i = 0
+        while i <= length:
+            if candidateOverlapsTarget(target, candidate, i):
+                largestOverlap = i
+                i += 1
+            else:
+                i += 1
+                continue
+        return largestOverlap
+    else:
+        return -1
 
-def findLargestOverlap():
-    return
 
-def findBestCandidate():
-    return
+def findBestCandidate(target, candidates):
+    largestStrand = ''
+    largestOverlap = 0
+    for candidate in candidates:
+        overlap = findLargestOverlap(target, candidate)
+        if findLargestOverlap(target, candidate) > largestOverlap:
+            largestStrand = candidate
+            largestOverlap = overlap
+        else:
+            continue
+    return largestStrand, largestOverlap
 
-def joinTwoStrands():
-    return
+
+def joinTwoStrands(target, candidate, overlap):
+    if candidateOverlapsTarget(target, candidate, overlap):
+        return target[0:len(target) - overlap] + candidate
+    else:
+        return target + candidate
+
 
 def main():
-    return
+    target = returnFirstString(fileToList(input('Target strand filename: ')))
+    candidate = fileToList(input('Candidate strands filename: '))
+
+    bestCandidate = findBestCandidate(target, candidate)
+    print(joinTwoStrands(target, bestCandidate[0], bestCandidate[1]))
+
+
+if __name__ == '__main__':
+    main()
