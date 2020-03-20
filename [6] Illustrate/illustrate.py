8a3ab439047f87febd0d6d015dc017f6c967861d
MatthewScottBennett <matt.bennett715@gmail.com>
2020-03-19 18:47:52 -0600

    changed folder names

--- [6] Illustrate/illustrate.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-class Illustrate:
-    def __init__ (self, width, height):
-        self.width =  width
-        self.height = height
-
-    def draw(self, surface):
-        return
