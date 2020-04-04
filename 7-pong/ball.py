09d1362fb1a33c9083e0754fc72f8810bd0d31f5
Matt Bennett <matt.bennett715@gmail.com>
2020-04-04 13:29:42 -0600

    Pong Collision

--- 7-pong/ball.py
+++ 7-pong/ball.py
@@ -1,5 +1,7 @@
 # http://cit.dixie.edu/cs/1410/labs/pong.php
 
+import pygame
+
 class Ball:
     def __init__(self, size, min_x, max_x, min_y, max_y, left_paddle_x, right_paddle_x):
         self.x = min_x
@@ -92,35 +94,68 @@ class Ball:
 
 
     def setLeftPaddleY(self, paddle_min_y, paddle_max_y):
-        return
+        if (paddle_min_y >= self.min_y) and (paddle_max_y <= self.max_y) and (paddle_min_y < paddle_max_y):
+            self.left_paddle_min_y = paddle_min_y
+            self.left_paddle_max_y = paddle_max_y
+            return True
+        return False
 
 
     def setRightPaddleY(self, paddle_min_y, paddle_max_y):
-        return
+        if (paddle_min_y >= self.min_y) and (paddle_max_y <= self.max_y) and (paddle_min_y < paddle_max_y):
+            self.right_paddle_min_y = paddle_min_y
+            self.right_paddle_max_y = paddle_max_y
+            return True
+        return False
 
 
     def checkTop(self, new_y):
-        return
+        if new_y < self.min_y:
+            self.dy = -self.dy
+            new_y = self.min_y - (new_y - self.min_y)
+        return new_y
 
 
     def checkBottom(self, new_y):
-        return
+        bottom = self.max_y - self.size
+        if new_y > bottom:
+            self.dy = -self.dy
+            new_y = bottom - (new_y - bottom)
+        return new_y
 
 
     def checkLeft(self, new_x):
-        return
+        if new_x < self.min_x:
+            new_x = self.min_x
+            self.dx = 0
+            self.dy = 0
+        return new_x
 
 
     def checkRight(self, new_x):
-        return
+        right = self.max_x - self.size
+        if new_x > right:
+            new_x = self.max_x - self.size
+            self.dx = 0
+            self.dy = 0
+        return new_x
 
 
     def checkLeftPaddle(self, new_x, new_y):
-        return
+        mid_y = (new_y + self.y)/2
+        if mid_y >= self.left_paddle_min_y and mid_y <= self.left_paddle_max_y and new_x <= self.left_paddle_x and self.x >= self.left_paddle_x:
+            self.dx = -self.dx
+            new_x = self.left_paddle_x + (self.left_paddle_x - new_x)
+        return new_x
 
 
     def checkRightPaddle(self, new_x, new_y):
-        return
+        right = self.right_paddle_x - self.size
+        mid_y = (new_y + self.y)/2
+        if mid_y >= self.right_paddle_min_y and mid_y <= self.right_paddle_max_y and new_x >= right and self.x <= right:
+            self.dx = -self.dx
+            new_x = right + (right - new_x)
+        return new_x
 
 
     def move(self, dt):
@@ -136,4 +171,5 @@ class Ball:
 
 
     def draw(self, surface):
-        return
+        r = pygame.Rect(self.x, self.y, self.size, self.size)
+        pygame.draw.rect(surface, (0,0,0), r)
